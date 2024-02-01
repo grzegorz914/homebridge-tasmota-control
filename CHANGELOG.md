fddb6a62b36ec42b70c5626cea458dde578a00be
Grzegorz <grzegorz914@icloud.com>
2024-02-01 08:06:48 +0100

    release 0.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.14.13] - (31.12.2022)
+## [0.5.0] - (01.02.2024)
+## Changes
+- add support for Tasmota FW. <= 12.1.1, thanks @xrayone912, [#5](https://github.com/grzegorz914/homebridge-tasmota-control/issues/5)
+- bump dependencies
+- cleanup
+
+## [0.4.13] - (31.12.2022)
 ## Changes
 - bump dependencies
 
---
+++ src/tasmotadevice.js
@@ -0,0 +1,196 @@
+'use strict';
+const fs = require('fs');
+const axios = require('axios');
+const CONSTANS = require('./constans.json');
+let Accessory, Characteristic, Service, Categories, UUID;
+
+class TasmotaDevice {
+    constructor(api, prefDir, config, log) {
+        this.api = api;
+        this.log = log;
+
+        Accessory = api.platformAccessory;
+        Characteristic = api.hap.Characteristic;
+        Service = api.hap.Service;
+        Categories = api.hap.Categories;
+        UUID = api.hap.uuid;
+
+        //device configuration
+        this.name = config.name;
+        this.host = config.host;
+        this.user = config.user;
+        this.passwd = config.passwd;
+        this.auth = config.auth || false;
+        this.refreshInterval = config.refreshInterval || 5;
+        this.enableDebugMode = config.enableDebugMode || false;
+        this.disableLogInfo = config.disableLogInfo || false;
+        this.disableLogDeviceInfo = config.disableLogDeviceInfo || false;
+
+        //get Device info
+        this.manufacturer = 'Tasmota';
+        this.modelName = 'Model Name';
+        this.serialNumber = 'Serial Number';
+        this.firmwareRevision = 'Firmware Revision';
+
+        //setup variables
+        this.powerStates = [];
+        this.names = [];
+        this.channelsCount = 0;
+        this.startPrepareAccessory = true;
+
+        const url = this.auth ? `http://${this.host}/cm?user=${this.user}&password=${this.passwd}&cmnd=` : `http://${this.host}/cm?cmnd=`
+        this.axiosInstance = axios.create({
+            method: 'GET',
+            baseURL: url,
+            timeout: 10000
+        });
+
+        //check if the directory exists, if not then create it
+        if (fs.existsSync(prefDir) == false) {
+            fs.mkdirSync(prefDir);
+        };
+
+        this.getDeviceInfo();
+    };
+
+    async reconnect() {
+        await new Promise(resolve => setTimeout(resolve, 15000));
+        this.getDeviceInfo();
+    };
+
+    async updateDeviceState() {
+        await new Promise(resolve => setTimeout(resolve, this.refreshInterval * 1000));
+        this.checkDeviceState();
+    };
+
+    async getDeviceInfo() {
+        this.log.debug(`Device: ${this.host} ${this.name}, requesting info.`);
+        try {
+            const deviceInfo = await this.axiosInstance(CONSTANS.ApiCommands.Status);
+            const debug = this.enableDebugMode ? this.log(`Device: ${this.host} ${this.name}, debug device info: ${JSON.stringify(deviceInfo.data, null, 2)}`) : false;
+
+            const deviceName = deviceInfo.data.Status.DeviceName;
+            const friendlyName = Array.isArray(deviceInfo.data.Status.FriendlyName) ? deviceInfo.data.Status.FriendlyName : [];
+            const friendlyNameCount = friendlyName.length;
+            const modelName = deviceInfo.data.StatusFWR.Hardware ?? '';
+            const addressMac = deviceInfo.data.StatusNET.Mac;
+            const firmwareRevision = deviceInfo.data.StatusFWR.Version;
+
+            if (!this.disableLogDeviceInfo) {
+                this.log(`----- ${this.name} -----`);
+                this.log(`Manufacturer: ${this.manufacturer}`);
+                this.log(`Hardware: ${modelName}`);
+                this.log(`Serialnr: ${addressMac}`);
+                this.log(`Firmware: ${firmwareRevision}`);
+                this.log(`Channels: ${friendlyNameCount}`);
+                this.log(`----------------------------------`);
+            };
+
+            this.modelName = modelName;
+            this.serialNumber = addressMac;
+            this.firmwareRevision = firmwareRevision;
+            this.friendlyName = friendlyName;
+            this.channelsCount = friendlyNameCount;
+
+            this.checkDeviceState();
+        } catch (error) {
+            this.log.error(`Device: ${this.host} ${this.name}, check info error: ${error}, trying to reconnect in 15s.`);
+            this.reconnect();
+        };
+    };
+
+    async checkDeviceState() {
+        this.log.debug(`Device: ${this.host} ${this.name}, requesting state.`, this.host, this.name);
+        try {
+            const friendlyName = this.friendlyName;
+            const channelsCount = this.channelsCount;
+            const deviceState = await this.axiosInstance(CONSTANS.ApiCommands.PowerStatus);
+            const debug = this.enableDebugMode ? this.log(`Device: ${this.host} ${this.name}, debug state: ${JSON.stringify(deviceState.data, null, 2)}`) : false;
+
+            for (let i = 0; i < channelsCount; i++) {
+                const power = channelsCount === 1 ? 'POWER' : 'POWER' + (i + 1);
+                const power1 = channelsCount === 1 ? 'POWER1' : 'POWER' + (i + 1);
+                const powerState = deviceState.data[power] !== undefined ? (deviceState.data[power] === 'ON') : (deviceState.data[power1] === 'ON');
+                const name = friendlyName[i];
+
+                if (this.tasmotaServices && (powerState !== this.powerStates[i])) {
+                    this.tasmotaServices[i]
+                        .updateCharacteristic(Characteristic.On, powerState);
+                };
+
+                const pushReplace = this.startPrepareAccessory ? this.powerStates.push(powerState) : this.powerStates[i] = powerState;
+                const pushReplace1 = this.startPrepareAccessory ? this.names.push(name) : this.names[i] = name;
+            };
+
+            //start prepare accessory
+            if (this.startPrepareAccessory && this.serialNumber) {
+                this.prepareAccessory();
+            };
+
+            this.updateDeviceState();
+        } catch (error) {
+            this.log.error(`Device: ${this.host} ${this.name}, check state error: ${error}, trying again.`);
+            this.updateDeviceState();
+        };
+    };
+
+    //Prepare accessory
+    prepareAccessory() {
+        this.log.debug('prepareAccessory');
+        const accessoryName = this.name;
+        const accessoryUUID = UUID.generate(this.serialNumber);
+        const accessoryCategory = Categories.OTHER;
+        const accessory = new Accessory(accessoryName, accessoryUUID, accessoryCategory);
+
+        //Prepare information service
+        this.log.debug('prepareInformationService');
+        const manufacturer = this.manufacturer;
+        const modelName = this.modelName;
+        const serialNumber = this.serialNumber;
+        const firmwareRevision = this.firmwareRevision;
+
+        accessory.getService(Service.AccessoryInformation)
+            .setCharacteristic(Characteristic.Manufacturer, manufacturer)
+            .setCharacteristic(Characteristic.Model, modelName)
+            .setCharacteristic(Characteristic.SerialNumber, serialNumber)
+            .setCharacteristic(Characteristic.FirmwareRevision, firmwareRevision);
+
+        //Prepare service 
+        this.log.debug('prepareTasmotaService');
+        this.tasmotaServices = [];
+        const channelsName = this.names;
+        const channelsCount = this.channelsCount;;
+        for (let i = 0; i < channelsCount; i++) {
+            const serviceName = channelsCount > 1 ? `${accessoryName} ${channelsName[i]}` : accessoryName;
+            const logName = channelsCount > 1 ? `${accessoryName}, channel: ${channelsName[i]}` : `${accessoryName}`
+            const tasmotaService = new Service.Outlet(serviceName, `tasmotaService${[i]}`);
+            tasmotaService.addOptionalCharacteristic(Characteristic.ConfiguredName);
+            tasmotaService.setCharacteristic(Characteristic.ConfiguredName, serviceName);
+            tasmotaService.getCharacteristic(Characteristic.On)
+                .onGet(async () => {
+                    const state = this.powerStates[i];
+                    const logInfo = this.disableLogInfo ? false : this.log(`Device: ${this.host} ${logName}, state: ${state ? 'ON' : 'OFF'}`);
+                    return state;
+                })
+                .onSet(async (state) => {
+                    const powerOn = channelsCount === 1 ? CONSTANS.ApiCommands.Power + CONSTANS.ApiCommands.On : CONSTANS.ApiCommands.Power + (i + 1) + CONSTANS.ApiCommands.On;
+                    const powerOff = channelsCount === 1 ? CONSTANS.ApiCommands.Power + CONSTANS.ApiCommands.Off : CONSTANS.ApiCommands.Power + (i + 1) + CONSTANS.ApiCommands.Off;
+                    state = state ? powerOn : powerOff;
+                    try {
+                        await this.axiosInstance(state);
+                        const logInfo = this.disableLogInfo ? false : this.log(`Device: ${this.host} ${logName}, set state: ${state ? 'ON' : 'OFF'}`);
+                    } catch (error) {
+                        this.log.error(`Device: ${this.host} ${logName}, set state error: ${error}`);
+                    }
+                });
+            this.tasmotaServices.push(tasmotaService);
+            accessory.addService(tasmotaService);
+        };
+
+        this.api.publishExternalAccessories(CONSTANS.PluginName, [accessory]);
+        this.log.debug(`Device: ${this.host} ${accessoryName}, publish as external accessory.`);
+        this.startPrepareAccessory = false;
+    };
+};
+
+module.exports = TasmotaDevice;
